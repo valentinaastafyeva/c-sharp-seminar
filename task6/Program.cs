c573708281888778de7ab069575f49fc75ea2bfc
valentina <astafjewa.valentina@gmail.com>
2022-09-22 00:04:22 +0300

    even or odd

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿// 6
+
+Console.WriteLine("Is a number even? Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2 == 0) Console.WriteLine($"Yes, {num} is even");
+else Console.WriteLine($"Oops, {num} is odd");
+
