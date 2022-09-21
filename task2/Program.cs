934a100f229e9b30c5d763876771819e87eccead
valentina <astafjewa.valentina@gmail.com>
2022-09-21 23:57:22 +0300

    great

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// 2
+
+Console.WriteLine("Which number is greater? Enter the first one: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second one: ");
+int num2 = Convert.ToInt32(Console.ReadLine());;
+if(num1 > num2) Console.WriteLine($"{num1} is greater than {num2}");
+else Console.WriteLine($"{num2} is greater than {num1}");
